682807463e3564d3748179a37c245521e1557c15
Rishikesh <rishikesh2511990@gmail.com>
2024-03-07 16:30:10 +0530

    Revert "7/3/24"
    This reverts commit 51dab0194313c67ebf3be52808fd3253679035c2.

--- code/newPackage/ex11.js
+++
@@ -1,2 +0,0 @@
-let greet=require('rishipackagetry');
-greet("RISHIKESH");
--- code/packageTry/index.js
+++
@@ -1,6 +0,0 @@
-let lowerCase = require("lower-case").lowerCase;
-function greet(name) {
-    console.log(`${lowerCase(`Hello ${ name }, welcome to coding!`)}`);
-}
-module.exports=greet;
-greet("rishi");
